26ec5a892ad2efc25c3d42415d2e5c7d23cae8ce
Anna Gudinskaya <ann.gudin@gmail.com>
2020-10-03 01:58:06 +0300

    feat: add solution of arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let sortedArray = array
+  if (sortedArray == false || !sortedArray || sortedArray == true && sortedArray.length === 0) {
+    return 0
+  }
+  sortedArray.sort((a, b) => a - b)
+  return sortedArray[0]
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let sortedArray = array
+  if (sortedArray == false || !sortedArray || sortedArray == true && sortedArray.length === 0) {
+    return 0
+  }
+  sortedArray.sort((a, b) => a - b)
+  return sortedArray[sortedArray.length - 1]
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array == false || !array || array == true && array.length === 0) {
+    return 0
+  }
+  let sum = 0
+  let i = 0
+  while (i < array.length) {
+    sum += array[i]
+    i += 1
+  }
+  return sum / array.length;
 }
